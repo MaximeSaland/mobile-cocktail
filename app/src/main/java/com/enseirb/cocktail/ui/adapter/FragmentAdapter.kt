243d91ea1a869602e962d91598653e19bc570939
Maxime Saland <maxsld.contact@gmail.com>
2023-11-14 23:29:19 +0100

    :sparkles: Added viewpager2 support + categories and ingredient fragment

--- app/src/main/java/com/enseirb/cocktail/ui/adapter/FragmentAdapter.kt
+++ app/src/main/java/com/enseirb/cocktail/ui/adapter/FragmentAdapter.kt
@@ -1,4 +1,27 @@
 package com.enseirb.cocktail.ui.adapter
 
-class FragmentAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.enseirb.cocktail.ui.categories.CategoriesFragment
+import com.enseirb.cocktail.ui.ingredients.IngredientsFragment
+import com.enseirb.cocktail.ui.search.SearchFragment
+
+class FragmentAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun createFragment(position: Int): Fragment {
+        when(position) {
+            0 -> return SearchFragment.newInstance()
+            1 -> return CategoriesFragment.newInstance()
+            2 -> return IngredientsFragment.newInstance()
+            else -> {
+                return SearchFragment.newInstance()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
 }
