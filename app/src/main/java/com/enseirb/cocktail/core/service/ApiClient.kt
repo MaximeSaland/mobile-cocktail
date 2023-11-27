e45baac9ca3b38c87ecd71cea1190af65426beae
Maxime Saland <maxsld.contact@gmail.com>
2023-11-27 15:38:14 +0100

    :construction:  added api files

---
+++ app/src/main/java/com/enseirb/cocktail/core/service/ApiClient.kt
@@ -0,0 +1,18 @@
+package com.enseirb.cocktail.core.service
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+    private val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
+    val service: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/enseirb/cocktail/core/service/ApiService.kt
@@ -0,0 +1,20 @@
+package com.enseirb.cocktail.core.service
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("search.php")
+    fun getCocktailsDetailsByCocktailName(@Query("s") name: String): Call<ApiResponse<Cocktail?>>
+    @GET("filter.php")
+    fun getCocktailsDetailsByIngredientName(@Query("i") name: String): Call<ApiResponse<Cocktail?>>
+    @GET("list.php?c=list")
+    fun getAllCategories(): Call<ApiResponse<StringResponse>>
+    @GET("list.php?c=list")
+    fun getAllIngredidients(): Call<ApiResponse<StringResponse>>
+    @GET("list.php?a=list")
+    fun getAllAlcoholic(): Call<ApiResponse<String>>
+    @GET("filter.php")
+    fun getCocktailsByCategory(@Query("c") category: String): Call<ApiResponse<Cocktail?>>
+}
